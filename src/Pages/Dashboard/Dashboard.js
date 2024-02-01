232b40813531407720e4647e3576a94a30502a4b
Rohit Pakhrin <roitpak@gmail.com>
2024-02-01 10:59:27 +0545

    Appwrite login setup

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,4 +1,28 @@
+import { Account, Client } from 'appwrite';
+import { useEffect } from 'react';
+
+const client = new Client();
+
+client
+  .setEndpoint(process.env.REACT_APP_APPWRITE_END_POINT)
+  .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID);
+const account = new Account(client);
+
 function Dashboard() {
+  useEffect(() => {
+    getEmailSession();
+  }, []);
+  const getEmailSession = async () => {
+    await account
+      .get()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return <div>This is a dashboard.</div>;
 }
 
--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -2,6 +2,14 @@ import { useEffect, useState } from 'react';
 import { Button, Checkbox, TextField } from '@mui/material';
 import { buttonStyle, textField, wrapperStyle } from './LoginStyles';
 import { useNavigate } from 'react-router-dom';
+import { Account, Client } from 'appwrite';
+
+const client = new Client();
+
+client
+  .setEndpoint(process.env.REACT_APP_APPWRITE_END_POINT)
+  .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID);
+const account = new Account(client);
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -12,7 +20,7 @@ function LoginPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('Variable set in .env', process.env.REACT_APP_MY_VARIABLE);
+    console.log('Variable set in .env', process.env.REACT_APP_APPWRITE_END_POINT);
     // eslint-disable-next-line no-useless-escape
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     if (emailRegex.test(email) && password.length > 7) {
@@ -21,8 +29,16 @@ function LoginPage() {
       setValidated(false);
     }
   }, [email, password]);
-  const onLoginPressed = () => {
-    navigate('/dashboard');
+  const onLoginPressed = async () => {
+    await account
+      .createEmailSession(email, password)
+      .then((data) => {
+        navigate('/dashboard');
+        console.log('Email login data--->', data);
+      })
+      .catch((err) => {
+        console.log('Error occured:', err);
+      });
   };
 
   return (
